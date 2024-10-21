f924d6d7180cabdb61986b2bcef382b5c524b8b7
Meghna Vyas <114874650+MegWho@users.noreply.github.com>
2024-10-21 18:59:44 +0530

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -3,7 +3,7 @@ const wishCount = document.getElementById('wishCount');
 const photoButton = document.getElementById('photoButton');
 const quizButton = document.getElementById('quizButton');
 
-let count = 0;
+let count = 25;
 
 wishButton.addEventListener('click', () => {
     count++;
